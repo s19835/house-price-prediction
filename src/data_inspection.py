0a217293a2037484e330c299ccb984ad34b8e9ea
s19835 <s19835@sci.pdn.ac.lk>
2025-02-22 14:33:39 +0530

    add: logger

--- src/data_inspection.py
+++ src/data_inspection.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+logger = logging.getLogger(__name__)
 
 # Base class
 class DataInspection(ABC):
@@ -26,8 +30,13 @@ class DataTypeInspection(DataInspection):
 
         returns: Prints data types in the data frame
         '''
-        print("Data Types in Data Frame: \n")
-        print(df.info())
+
+        if isinstance(df, pd.DataFrame):
+            logger.error("Input must be a pandas data frame")
+            return
+
+        logger.info("=== Data Types in Data Frame ===")
+        logger.info("\n" + str(df.info()))
 
 class DataSummaryStatistics(DataInspection):
     def inspect_data(self, df: pd.DataFrame):
@@ -40,8 +49,45 @@ class DataSummaryStatistics(DataInspection):
         returns:
         summary statistic of data frame
         '''
-        print("Summary statistics (Numerical Features): \n")
-        print(df.describe())
-        print("Summary statistics (Categorical Features): \n")
-        print(df.describe(include=[object]))
+        if not isinstance(df, pd.DataFrame):
+            logger.error("Input must be a pandas data frame")
+            return
+        
+        logger.info("=== Summary statistics ===")
+        logger.info("\nNumerical Features: ")
+        logger.info(df.describe())
+        
+        logger.info("\nCategorical Features: ")
+        logger.info(df.describe(include=[object]))
 
+# context class (to use diff statergies)
+class DataInspector:
+    def __init__(self, strategy: DataInspection):
+        '''
+        Initialize the data inspector with specific strategy
+
+        parameters: 
+        strategy (DataInspection): The inspection strategy to use
+        '''
+        self._strategy = strategy
+    
+    def set_strategy(self, strategy: DataInspection):
+        '''
+        set new data inspection strategy to use
+
+        parameters:
+        strategy (DataInspection): The new inspection strategy to use
+        '''
+        self._strategy = strategy
+    
+    def inspect(self, df: pd.DataFrame) -> None:
+        '''
+        perform data inspection using the strategy
+
+        parameters:
+        df (pd.DataFrame): dataframe to be inspected
+
+        return:
+        print statement of stratgy out put (Summarized Data)
+        '''
+        self._strategy.inspect_data(df)
