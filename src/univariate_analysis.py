12cc296cd26c98262a1886d7ad0f7be83a81e861
s19835 <s19835@sci.pdn.ac.lk>
2025-02-23 07:03:31 +0530

    add: univariate file

---
+++ src/univariate_analysis.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+logger = logging.getLogger(__name__)
+
