e623e915122a3040e152172d47be23f961e810bc
s19835 <s19835@sci.pdn.ac.lk>
2025-02-23 08:51:42 +0530

    Add: Numerical Univariate Analysis

--- src/univariate_analysis.py
+++ src/univariate_analysis.py
@@ -8,3 +8,80 @@ import logging
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
 
+# base class (interface)
+class UnivariateAnalysis(ABC):
+    @abstractmethod
+    def analyze(self, df: pd.DataFrame, feature: str):
+        '''
+        perform univariate analysis in a single column of data
+
+        parameters:
+        df (pd.DataFrame): The data frame that feature exist
+        feature (str): feature that you are going to analyze
+
+        return:
+        None: visualize the distribution of the feature
+        '''
+        pass
+
+# concrete classes
+class NumericalUnivariateAnalysis(UnivariateAnalysis):
+    def analyze(self, df: pd.DataFrame, feature: str):
+        '''
+        perform univariate analysis on numerical data
+
+        parameters:
+        df (pd.DataFrame): The data frame that feature exist
+        feature (str): feature that you are going to analyze
+
+        return:
+        None: display a histogram with kde
+        '''
+        logger.info("=== Numerical Univariate Analysis ===")
+        
+        sns.histplot(data=df[feature], kde=True, bins=30)
+        
+        plt.title(f"Distribution of {feature}")
+        plt.xlabel('feature')
+        plt.ylabel("frequency")
+        plt.show()
+
+class CategoricalUnivariateAnalysis(UnivariateAnalysis):
+    def analyze(self, df: pd.DataFrame, feature: str):
+        '''
+        
+        '''
+        pass
+
+# context class
+class UnivariateAnalyzer:
+    def __init__(self, strategy: UnivariateAnalysis):
+        '''
+        initialize Univariate Analyze with specific strategy
+
+        parameters:
+        strategy (UnivariateAnalysis): strategy to use
+        '''
+        self.strategy = strategy
+    
+    def set_strategy(self, strategy: UnivariateAnalysis):
+        '''
+        set a new strategy to use
+
+        parameters:
+        strategy (UnivariateAnalysis): new strategy to set
+        '''
+        self.strategy = strategy
+    
+    def analyze(self, df: pd.DataFrame, feature: str):
+        '''
+        perform univariate analyze using current strategy
+
+        parameters:
+        df (pd.DataFrame): data frame that need to analyze
+        feature (str): numerical feature to analyze
+
+        return:
+        None: Provide visualized plots for univariate data
+        '''
+        self.strategy.analyze(df, feature)
