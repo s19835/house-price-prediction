94e7d0ef2297427a9f24a9f9001d36aceddaf47b
s19835 <s19835@sci.pdn.ac.lk>
2025-02-24 20:36:54 +0530

    add load data step

---
+++ steps/load_data_step.py
@@ -0,0 +1,21 @@
+from src.load_data import DataProcessorFactory
+from zenml import step
+
+import pandas as pd
+
+@step
+def data_load_step(file_path: str) -> pd.DataFrame:
+    '''
+    load data from file as pandas dataframe
+
+    parameters:
+    file_path (str): path to the file
+
+    return:
+    pandas data frame with data from files
+    '''
+    file_extension = file_path[file_path.rfind('.'):]
+    raw_data = DataProcessorFactory.get_processor(file_extension)
+
+    df = raw_data.load_data("../data/raw/train.csv")
+    return df
