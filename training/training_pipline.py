927d96989094ac28a11e52235a6ae5507250aeb1
s19835 <s19835@sci.pdn.ac.lk>
2025-02-26 21:04:02 +0530

    add: feature engineering pipline

--- training/training_pipline.py
+++ training/training_pipline.py
@@ -1,6 +1,7 @@
 from zenml import Model, pipeline
 from steps.load_data_step import data_load_step
 from steps.missing_value_handling_step import missing_value_handling_step
+from steps.feature_engineering_step import feature_engineering_step
 
 @pipeline(
     model=Model(
@@ -18,3 +19,8 @@ def ml_pipline():
 
     # handling missing values
     handled_data = missing_value_handling_step(data)
+
+    # feature engineering
+    engineered_data = feature_engineering_step(
+        handled_data, strategy='log', features=['GrLivArea', 'SalePrice']
+    )
