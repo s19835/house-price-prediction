d4b7897f7d262e5071b3e40d79c1015478065912
s19835 <s19835@sci.pdn.ac.lk>
2025-02-26 18:29:07 +0530

    add pipline running

---
+++ run_pipline.py
@@ -0,0 +1,22 @@
+import click
+from training.training_pipline import ml_pipline
+from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
+
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+logger = logging.getLogger(__name__)
+
+@click.command()
+def main():
+    """
+    Run the ML flow Pipline and kick strat the MLflow UI dashborad for experiment tracking
+    """
+
+    # run the pipline
+    run = ml_pipline()
+
+    logger.info(f"\nmlflow UI --backend-stroe-ui '{get_tracking_uri()}'\n")
+
+if __name__ == "__main__":
+    main()
