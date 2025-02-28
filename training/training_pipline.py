193578f2f82e7f42c1a4833448424b350b8bdc89
s19835 <s19835@sci.pdn.ac.lk>
2025-02-28 17:50:59 +0530

    cleaned data - outlier detection pipline

--- training/training_pipline.py
+++ training/training_pipline.py
@@ -2,6 +2,7 @@ from zenml import Model, pipeline
 from steps.load_data_step import data_load_step
 from steps.missing_value_handling_step import missing_value_handling_step
 from steps.feature_engineering_step import feature_engineering_step
+from steps.outlier_detection_step import outlier_detection_step
 
 @pipeline(
     model=Model(
@@ -24,3 +25,6 @@ def ml_pipline():
     engineered_data = feature_engineering_step(
         handled_data, strategy='log', features=['GrLivArea', 'SalePrice']
     )
+
+    # outlier detection
+    clearned_data = outlier_detection_step(engineered_data, column_name='SalePrice')
